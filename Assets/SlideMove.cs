569b894d730bc72eb0458d450493a924b9db2761
Lee Jang Gun <37167629+gun7992@users.noreply.github.com>
2020-06-01 15:24:15 +0900

    initial

---
+++ Assets/SlideMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount == 1)
+        {
+        	transform.Translate(Input.GetTouch(0).deltaPosition[0], 0, 0);
+        }
+    }
+}
---
+++ Temp/gradleOut/launcher/build/generated/source/buildConfig/release/com/DefaultCompany/Sea_Story/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.DefaultCompany.Sea_Story;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.DefaultCompany.Sea_Story";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
